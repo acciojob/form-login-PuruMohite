85373782b260a287539b127225f5ddade622ac72
PuruMohite <107110840+PuruMohite@users.noreply.github.com>
2026-05-30 14:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+let firstName = document.querySelector('input[name="fname"]');
+let lastName = document.querySelector('input[name="lname"]');
+let submitBtn = document.querySelector('input[type="submit"]');
+
 function getFormvalue() {
     //Write your code here
-
+	let firstNameValue = firstName.value.trim();
+    let lastNameValue = lastName.value.trim();
+    alert(`${firstNameValue} ${lastNameValue}`);
 }
+
